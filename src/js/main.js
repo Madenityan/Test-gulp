e9e423fce199f80a29689a05664121b7bcc3e413
madenityan <marinakorsyn@gmail.com>
2017-12-27 20:56:48 +0200

    added category section

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,18 @@
 $(document).ready(function() {
-    $(window).on("scroll",function(){
-        if (($(window).scrollTop() >= 50) && ($(window).width() >= 992) ) {
-            $('.header-wrapper').addClass('fixed');
 
-        } else {
-            $('.header-wrapper').removeClass('fixed');
-        }
+    var $container = $(".masonry-container");
+    $container.imagesLoaded(function() {
+        $container.masonry( {
+            columWidht: ".item",
+            itemSelector: ".item"
+            });
+         });
     });
 
 
-});
+
+
+
+
+
+
